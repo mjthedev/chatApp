166ae2c9086d14ebcedc190fe70d6dc8f681af48
mjthedev <i@marrionjohnson.io>
2022-09-30 07:43:59 -0600

    initiated react app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './style/index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="main">chatapp-webapp</h1>
+    </div>
+  );
+}
+
+export default App;
